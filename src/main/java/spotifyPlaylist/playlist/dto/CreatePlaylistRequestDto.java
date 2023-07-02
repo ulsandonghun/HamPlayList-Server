076aa111d7812ddeb51f8afbeee55806655332c2
MIN0225 <cro77@naver.com>
2023-07-02 16:23:18 +0900

    feat: 페이지(플레이리스트) 생성

---
+++ src/main/java/spotifyPlaylist/playlist/dto/CreatePlaylistRequestDto.java
@@ -0,0 +1,10 @@
+package spotifyPlaylist.playlist.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreatePlaylistRequestDto {
+    private String playlistName;
+}
--- src/main/java/spotifyPlaylist/playlist/service/PlaylistService.java
+++ src/main/java/spotifyPlaylist/playlist/service/PlaylistService.java
@@ -1,5 +1,8 @@
 package spotifyPlaylist.playlist.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import spotifyPlaylist.playlist.domain.Playlist;
 import spotifyPlaylist.playlist.domain.PlaylistSong;
 import spotifyPlaylist.playlist.domain.Sticker;
@@ -26,12 +29,29 @@ public class PlaylistService {
     private final PlaylistSongRepository playlistSongRepository;
     private final StickerRepository stickerRepository;
 
+    private static final Logger log = LoggerFactory.getLogger(PlaylistService.class);
+
     public PlaylistService(SpotifyService spotifyService, PlaylistRepository playlistRepository, UserRepository userRepository, PlaylistSongRepository playlistSongRepository, StickerRepository stickerRepository) {
         this.spotifyService = spotifyService;
         this.playlistRepository = playlistRepository;
         this.userRepository = userRepository;
         this.playlistSongRepository = playlistSongRepository;
         this.stickerRepository = stickerRepository;
+    }
+
+    @Transactional
+    public Playlist createPlaylist(Long userId, String playlistName) {
+        System.out.println("createPlaylist시작 ");
+        Optional<User> user = userRepository.findById(userId);
+        if (!user.isPresent()) {
+            throw new IllegalArgumentException("User not found with id: " + userId);
+        }
+        System.out.println("createPlaylist테스트 " + user.get().getUserId());
+
+        Playlist playlist = new Playlist();
+        playlist.setUser(user.get());
+        playlist.setPlaylistName(playlistName);
+        return playlistRepository.save(playlist);
     }
 
     @Transactional
@@ -58,6 +78,9 @@ public class PlaylistService {
             sticker.setPlaylistSong(playlistSong);
             sticker.setImgUrl(addSongRequestDto.getImageUrl());
             stickerRepository.save(sticker);
+
+            // Print the state of the Sticker entity
+            log.info("Sticker: " + sticker);
         }
     }
 }
